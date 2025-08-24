a942df53d4a399d795addf30b3c233b8f766eba3
Satatya-Mandal <satatya77@gmail.com>
2025-08-24 23:17:57 +0530

    Initial commit

--- food-del/backend/config/db.js
+++ food-del/backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('{ Add your mongodb URI Here }/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://satatya77:123456satatya@cluster0.r9vg5gx.mongodb.net/food del').then(()=>console.log("DB Connected"));
    
 }
 
